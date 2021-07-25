75bf1718b7dd8750ddbc4a6a7881a4c21854991c
Jesse Nguyen <jesse_nguyen9@yahoo.com>
2021-07-25 12:51:16 +1000

    Error handling for timer command

--- app/bot.py
+++ app/bot.py
@@ -13,19 +13,23 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 
-@bot.command(name="timer", help=f"Creates a timer: {PREFIX}timer <number minutes> [title]")
-async def timer(ctx, mins, title="Timer"):
-    try:
-        mins = int(mins)
-    except:
-        return await ctx.reply("Invalid number")
-
-    if mins <= 0 or mins > 30:
-        return await ctx.send("Please keep minutes between 1 - 30")
+@bot.command(name="timer", help=f"Creates a timer: {PREFIX}timer <minutes> [title]")
+async def timer(ctx, minutes:int, title="Timer"):
+    if minutes <= 0 or minutes > 30:
+        raise commands.errors.BadArgument
         
-    timer = Timer(ctx, int(mins), title)
+    timer = Timer(ctx, int(minutes), title)
 
     await timer.start_timer()
+
+@timer.error
+async def timer_error(ctx, error):
+    if isinstance(error, commands.errors.MissingRequiredArgument):
+        await ctx.reply("\"minutes\" is a required argument that is missing.")
+    elif isinstance(error, commands.errors.BadArgument):
+        await ctx.reply("\"minutes\" argument must be a number between 1 and 30.")
+
+
 
 bot.run(TOKEN)
 
